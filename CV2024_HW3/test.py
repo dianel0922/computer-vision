c8e45c7383db7c57fa819ec2372466af483035ef
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2024-11-04 20:43:21 +0800

    up

--- CV2024_HW3/test.py
+++ CV2024_HW3/test.py
@@ -40,9 +40,10 @@ def detect_and_match_features(img1, img2, feature='SIFT'):
     matches = match(descriptors1, descriptors2)
     good_matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
     
-
+    
     img_matches = cv2.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
+    
+    img_matches = cv2.cvtColor(img_matches, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=(12, 6))
     plt.imshow(img_matches)
     plt.title("Feature Matching")
@@ -84,9 +85,6 @@ def ransac_homography(keypoints1, keypoints2, good_matches, threshold=5.0, max_i
             
             projected_pt1 = H @ pt1
             projected_pt1 /= projected_pt1[2]  
-            print("error:")
-            print(projected_pt1[:2])
-            print(pt2[:2])
             error = np.linalg.norm(projected_pt1[:2] - pt2[:2])
             if error < threshold:
                 inliers += 1
@@ -113,32 +111,54 @@ def ransac_homography(keypoints1, keypoints2, good_matches, threshold=5.0, max_i
    
     return ransac_homography(good_matches, keypoints1, keypoints2, threshold=5.0, max_iters=1000)
     
-    
-     
-
 
 def warp_and_stitch(img1, img2, H):
     h2, w2 = img2.shape[:2]
-    
     corners_img1 = np.float32([[0, 0], [0, img1.shape[0]], [img1.shape[1], img1.shape[0]], [img1.shape[1], 0]]).reshape(-1, 1, 2)
-    warped_corners_img1 = cv2.perspectiveTransform(corners_img1, H)
+    
+    corners_img1_enpand = np.concatenate((corners_img1, np.ones((4, 1, 1))), axis=2)
+    warped_corners_img1 = []
+    for i in range(len(corners_img1_enpand)):
+        temp = H @ corners_img1_enpand[i].T
+        temp /= temp[2,0]
+        warped_corners_img1.append(temp[0:2].T)
+    
     corners = np.concatenate((warped_corners_img1, np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)), axis=0)
-
     [x_min, y_min] = np.int32(corners.min(axis=0).ravel() - 0.5)
     [x_max, y_max] = np.int32(corners.max(axis=0).ravel() + 0.5)
     translation_dist = [-x_min, -y_min]
     H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
 
     result = cv2.warpPerspective(img1, H_translation @ H, (x_max - x_min, y_max - y_min))
+    
+    result = np.zeros((y_max - y_min, x_max - x_min, 3))
+    temp = H_translation @ H
+    for i in range(len(img1)):
+        for j in range(len(img1[0])):
+            newtemp = temp @ np.array([[j],[i],[1]])
+            newtemp /= newtemp[2,0]
+            result[int(newtemp[1,0]),int(newtemp[0,0])] = img1[i,j]
+    temp = result
+
+    for i in range(len(temp)):
+        for j in range(len(temp[0])):
+            if temp[i,j].sum() == 0:
+                count = 0
+                for x in range(-1,2):
+                    for y in range(-1,2):
+                        if result[min(len(temp)-1,max(0,i+x)),min(len(temp[0])-1,max(0,j+y))].sum() != 0 :
+                            count += 1
+                            result[i,j] += result[min(len(temp)-1,max(0,i+x)),min(len(temp[0])-1,max(0,j+y))]/25
+                if count != 0:
+                    result[i,j] /= count
+                    result[i,j] *= 25
+    
     result[translation_dist[1]:h2 + translation_dist[1], translation_dist[0]:w2 + translation_dist[0]] = img2
-
-    return result
-
+    return result.astype("uint8")
 
 
-
-img1 = cv2.imread('data/hill1.jpg', cv2.COLOR_BGR2RGB)
-img2 = cv2.imread('data/hill2.jpg', cv2.COLOR_BGR2RGB)
+img1 = cv2.imread('data/t1.jpg', cv2.COLOR_RGB2BGR)
+img2 = cv2.imread('data/t2.jpg', cv2.COLOR_RGB2BGR)
 
 features = ['SIFT', 'MSER']
 # step 1,2
@@ -152,6 +172,7 @@ print(H)
 
 # step 4
 result = warp_and_stitch(img1, img2, H)
+result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
 
 plt.figure(figsize=(10, 5))
 plt.imshow(result, cmap='gray')
