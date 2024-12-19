d4e019278502734742e6060afe088e7dd4dee653
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2024-12-19 22:44:48 +0800

    Revert "hw4 terminal"
    This reverts commit 1cc88cc9f81f9d677311d3d46aea174ddc1c27c7.

---
+++ CV2024_HW4/CV2024_HW4/py2mat.m
@@ -0,0 +1,22 @@
+%記得改path
+dataPath = './output/SantaHat/'; 
+img_path = './my_data'; % data or mydata
+img_name = {'Mesona1.JPG'; 
+            'Mesona2.JPG'; 
+            'Statue1.bmp'; 
+            'Statue2.bmp';
+            'SantaHat1.jpg';
+            'SantaHat2.jpg'};
+
+points3dFile = fullfile(dataPath, 'points_3d.txt');
+points2dFile = fullfile(dataPath, 'points_2d.txt');
+cameraMatrixFile1 = fullfile(dataPath, 'camera_matrix1.txt');
+cameraMatrixFile2 = fullfile(dataPath, 'camera_matrix2.txt');
+
+points3d = readmatrix(points3dFile);
+points2d = readmatrix(points2dFile);
+cameraMatrix1 = readmatrix(cameraMatrixFile1);
+cameraMatrix2 = readmatrix(cameraMatrixFile2);
+%記得改圖片序號
+obj_main(points3d, points2d, cameraMatrix1,fullfile(img_path, img_name{5}), 5, dataPath);
+obj_main(points3d, points2d, cameraMatrix2, fullfile(img_path, img_name{6}), 6, dataPath);
